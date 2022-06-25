3b5a4e9fb75f0f6e2ba2e4c0880857b02c8911fe
znichola <znichola@student.42lausanne.ch>
2022-06-25 13:36:06 +0200

    header with typedef struct

---
+++ include/header.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znichola <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 13:25:14 by znichola          #+#    #+#             */
+/*   Updated: 2022/06/25 13:25:19 by znichola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+/*stors the input number split into goups of 3 dgits*/
+typedef struct s_num
+{
+	int	h;
+	int	k;
+	int	m;
+	int	b;
+
+}	t_sum;
+
+#endif
