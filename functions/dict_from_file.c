54a756a28f0223e041eab75fe63038fb4ec189df
Hexa_Core <hexa_core@bluewin.ch>
2022-06-25 21:22:21 +0200

    dict_from_file v0.1a

--- functions/dict_from_file.c
+++ functions/dict_from_file.c
@@ -6,7 +6,31 @@
 /*   By: matwinte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 20:37:02 by matwinte          #+#    #+#             */
-/*   Updated: 2022/06/25 20:37:03 by matwinte         ###   ########.fr       */
+/*   Updated: 2022/06/25 21:20:47 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "header.h"
+
+t_dict	*dict_from_file(char *filename)
+{
+	t_dict			*dict;
+	t_dict			*next;
+	char			*str;
+	
+	str = read_file(filename);
+	if (!str || !line_is_valid(str))
+		return (0);
+	dict = dict_create_elem(line_to_words, line_to_num);
+	if (!dict)
+		return (0);
+	next = dict;
+	while (str)
+	{
+		if (!line_is_valid(str))
+			return (0);
+		next = dict_append_elem(next, line_to_words(str), line_to_num(str));
+		str += line_to_end(str) + 1;	
+	}
+	return (dict);
+}
