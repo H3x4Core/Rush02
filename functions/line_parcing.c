91b1b0f7fe3bf85e1167310c9943fd7e84ca1478
znichola <znichola@student.42lausanne.ch>
2022-06-25 20:14:41 +0200

    line is valid, validates correctly.. I think

--- functions/line_parcing.c
+++ functions/line_parcing.c
@@ -14,28 +14,31 @@
 
 int	line_is_valid(char *str)
 {
-	if (ft_is_numeric(*str))
-	{
-		while (ft_is_numeric(*str))
+	char	*oldpos;
+
+	oldpos = str;
+	while (ft_is_numeric(*str))
 			str++;
-		while (*str == ' ')
-			str++;
-		if (*str == ':')
-		{
-			str++;
-			while (*str == ' ')
-				str++;
-			if (ft_is_printable(*str))
-			{
-				while (ft_is_printable(*str))
-					str++;
-				if (*str == '\n')
-					return (1);
-			}
-		}	
-	}
+	while (oldpos < str && ft_isspace(*str))
+		str++;
+	if (oldpos == str)
+		return (0);
+	if (*str != ':')
+		return (0);
+	str++;
+	if (ft_is_printable(*str))
+		return (1);
 	return (0);
 }
+	// while (ft_isspace(*str))
+	// 	str++;
+	// printf("got here[%c]\n", *str);
+	// while (ft_is_printable(*str))
+	// {
+	// 	str++;
+	// 	if (*str == '\n' || *str == '\0')
+	// 		return (1);
+	// }
 
 int	line_to_num(char *str)
 {
@@ -79,24 +82,12 @@ char *line_to_words(char *str)
 	return (words);
 }
 
-/*
+
 int main(int ac, char **av)
 {
-	//ac -= 1;
-	//av += 1;
-	for (int i = 0; i < ac; i++)
-	{
-		t_dict entry1;
-
-		entry1.n = line_to_num(av[i]);
-		entry1.words = line_to_words(av[i]);
-	}
-	t_dict entry1;
-
-	entry1.n = line_to_num(av[1]);
-	entry1.words = line_to_words(av[1]);
 	printf("%d\n", line_to_num(av[1]));
 	printf("%s\n", line_to_words(av[1]));
+	printf("line valid:%d\n", line_is_valid(av[1]));
 	return 0;
 }
-*/
+
