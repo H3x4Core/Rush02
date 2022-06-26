66f8bcbef4b4d2d7fccc558939e37ad96ebbf73c
Hexa_Core <hexa_core@bluewin.ch>
2022-06-26 20:33:59 +0200

    add header.h and remove main ft

--- functions/ft_atoi.c
+++ functions/ft_atoi.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: znichola <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 21:38:57 by znichola          #+#    #+#             */
-/*   Updated: 2022/06/16 21:39:04 by znichola         ###   ########.fr       */
+/*   Updated: 2022/06/26 20:31:55 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "header.h"
 
 int	ft_is_numeric(char c)
 {
@@ -47,18 +49,3 @@ long long int	ft_atoi(char *str)
 	}
 	return (n * s);
 }
-
-/*
-//gcc ft_atoi.c && ./a.out -234 13 +0 +123 -0 123.4 ++123 -+123 +-12 -13b23 -b12
-#include <stdlib.h>
-#include <stdio.h>
-int main(int ac, char **av)
-{
-	ac -= 1;
-	av += 1;
-	for (int i = 0; i < ac; i++)
-		printf("atoi:%d		ft_atoi:%lld\n", atoi(av[i]), ft_atoi(av[i]));
-	// printf("%s:	%d", av[ac], ft_atoi("-1112wq32"));
-	return (0);
-}
-*/
