c7523f4cd22b3dad0143906996e056b21cc90436
Hexa_Core <hexa_core@bluewin.ch>
2022-06-26 19:04:56 +0200

    cleaned main

--- main.c
+++ main.c
@@ -6,48 +6,12 @@
 /*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 10:13:36 by znichola          #+#    #+#             */
-/*   Updated: 2022/06/26 18:25:08 by matwinte         ###   ########.fr       */
+/*   Updated: 2022/06/26 19:04:02 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-<<<<<<< HEAD
-int	check_validity(char *str)
-{
-	int	i;
-	int	new_line_flag;
-
-	if (!str)
-		return (0);
-	new_line_flag = 1;
-	i = 0;
-	while (str[i])
-	{
-		if (new_line_flag)
-			if (! (line_is_valid(&str[i])))
-				return (0);
-		if (str[i] == '\n')
-			new_line_flag = 1;
-		else
-			new_line_flag = 0;
-		i++;
-	}
-	return (1);
-}
-
-int	get_value_from_entry(void)
-{
-	int		ret;
-	char	*str;
-
-	str = malloc(10000);
-	ret = read(0, str, 30720);
-	return (ft_atoi(str));
-}
-
-=======
->>>>>>> c67e0b0 (cleaned main -> main_helper, updated header)
 int	main(int argc, char **argv)
 {
 	char			*filename;
@@ -75,30 +39,3 @@ int	main(int argc, char **argv)
 	ft_putstr("\n");
 	return (0);
 }
-<<<<<<< HEAD
-
-void	translate(char *filename, long long int value)
-{
-	t_dict			*dict;
-	t_num			s_split;
-	unsigned int	*int_array;
-	char			**end_str;
-	int				i;
-
-	int_array = (unsigned int *)malloc(17 * sizeof(int));
-	dict = dict_from_file(filename);
-	dict_sort(dict);
-	s_split = split_chunks(value);
-	int_array = nums_as_ints(s_split, int_array);
-	end_str = humanize(int_array, dict);
-	i = 0;
-	while (end_str[i])
-	{
-		ft_putstr(end_str[i]);
-		ft_putstr(" ");
-		i++;
-	}
-	free(int_array);
-}
-=======
->>>>>>> c67e0b0 (cleaned main -> main_helper, updated header)
